8c039cf09ee769cb1d71064997c28d999e6240f6
opermancode <jagtapomkar3052002@gmail.com>
2026-03-27 18:46:04 +0530

    Create MainActivity.java

---
+++ android/app/src/main/java/com/locasdot/personal/MainActivity.java
@@ -0,0 +1,11 @@
+package com.locasdot.personal;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  @Override
+  protected String getMainComponentName() {
+    return "locasdotpersonal";
+  }
+}
