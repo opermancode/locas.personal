ca30608ddf245b35b70c90805143a7fd51f1d2ba
opermancode <jagtapomkar3052002@gmail.com>
2026-03-27 18:46:45 +0530

    Create MainApplication.java

---
+++ android/app/src/main/java/com/locasdot/personal/MainApplication.java
@@ -0,0 +1,35 @@
+package com.locasdot.personal;
+
+import android.app.Application;
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost =
+      new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+          return false;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+          return List.of(new MainReactPackage());
+        }
+
+        @Override
+        protected String getJSMainModuleName() {
+          return "index";
+        }
+      };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+}
